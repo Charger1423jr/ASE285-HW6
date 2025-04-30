baea9a57baca3dc0e22e9f52dcde5ccdaa2f6cd2
Preston Jackson <156366687+Charger1423jr@users.noreply.github.com>
2025-04-29 21:42:44 -0400

    Add files via upload

---
+++ code/acceptance.bat
@@ -0,0 +1,15 @@
+node src/passwordjs.js password.enc.txt sm.cho@hello.com 123456
+node src/passwordjs.js password.enc.txt john.deacon@good.com bestpassword
+node src/passwordjs.js password.enc.txt alan.may@best.com mypassword
+node src/passwordjs.js password.enc.txt henry.taylor@edu.com educatorbest
+node src/passwordjs.js password.enc.txt tim.allen@laff.com improvement
+node src/passwordjs.js password.enc.txt leadDev@msoft.com microsoft1
+node src/passwordjs.js password.enc.txt randyLewis@gmail.com password123
+node src/passwordjs.js password.enc.txt ted.gordon@outlook.com mypassword
+node src/passwordjs.js password.enc.txt henry.john@yahoo.com ihearttv
+node src/passwordjs.js password.enc.txt mr.incognito@deepweb.org mystery
+
+node src/passwordjs.js password.enc.txt sm.cho@hello.com 1234567
+node src/passwordjs.js password.enc.txt henry.taylor@edu.com educatorbests
+node src/passwordjs.js password.enc.txt fake.user@unknown.com somepassword
+node src/passwordjs.js password.enc.txt alan.may@best.com
